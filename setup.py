de2f3b9301a49c5c9179fc66464c000e0f5cc519
adam nunez <adam.a.nunez@gmail.com>
2017-06-11 22:50:01 -0500

    Created setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+	name = 'ipsy',
+	version = '0.1',
+	description = 'Command line IPS (Internal Patch System) applier and differ',
+	author = 'Adam Nunez',
+	author_email = 'adam.a.nunez@gmail.com',
+	license = 'GPLv3',
+	url = 'https://github.com/aanunez/ipsy',
+	packages = ['ipsy'],
+	scripts = ['ipsy/ipsy']
+)
